6bda3dab309f0df6f573e6022718264241f542ed
CannibalChoco <fudgeblimp@gmail.com>
2017-05-10 15:44:14 +0300

    toString() method and method comments

--- app/src/main/java/com/example/android/reportcard/MainActivity.java
+++ app/src/main/java/com/example/android/reportcard/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
         myReportCard.setMathGrade(2);
         myReportCard.setLiteratureGrade(9);
         myReportCard.setLiteratureGrade(1);
-        myReportCard.setLatvianGrade(9);
-        myReportCard.setArtGrade(7);
         myReportCard.setEnglishGrade(8);
         myReportCard.setEnglishGrade(8);
         myReportCard.setBiologyGrade(10);
@@ -27,14 +25,7 @@ public class MainActivity extends AppCompatActivity {
         myReportCard.setPhysicalEducationGrade(5);
         myReportCard.setPhysicalEducationGrade(5);
 
-        Log.v("ReportCard", "Math final grade = " + myReportCard.getMathFinalGrade());
-        Log.v("ReportCard", "Literature final grade = " + myReportCard.getLiteratureFinalGrade());
-        Log.v("ReportCard", "Latvian final grade = " + myReportCard.getLatvianFinalGrade());
-        Log.v("ReportCard", "Art final grade = " + myReportCard.getArtFinalGrade());
-        Log.v("ReportCard", "English final grade = " + myReportCard.getEnglishFinalGrade());
-        Log.v("ReportCard", "Biology final grade = " + myReportCard.getBiologyFinalGrade());
-        Log.v("ReportCard", "Music final grade = " + myReportCard.getMusicFinalGrade());
-        Log.v("ReportCard", "Physica education final grade = " + myReportCard.getPhysicalEducationFinalGrade());
+        Log.v("ReportCard", myReportCard.toString());
 
     }
 }
--- app/src/main/java/com/example/android/reportcard/ReportCard.java
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 /**
  * Created by Kristine on 08.05.2017.
+ *
+ * Java class of a Report Card. Class methods check for empty ArrayLists
+ * to prevent the app from crashing.
  */
 
 public class ReportCard {
@@ -27,11 +30,176 @@ public class ReportCard {
     }
 
     /**
-     * Gets the final int value of a subject.
+     * Gets all Math grades
      *
-     * @return final grade
+     * @return a string with Math grades
+     * or "no grades" if no grades have been added
      */
-    public int getMathFinalGrade(){
+    public String getMathGrades(){
+        String grades = "Math: ";
+
+        if (Math.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Math.size(); i < n; i++){
+            grades += Math.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all Literature grades
+     *
+     * @return a string with all Literature grades
+     * or "no grades" if no grades have been added
+     */
+    public String getLiteratureGrades(){
+        String grades = "Literature: ";
+
+        if (Literature.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Literature.size(); i < n; i++){
+            grades += Literature.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all Latvian grades
+     *
+     * @return a string with all Latvian grades
+     * or "no grades" if no grades have been added
+     */
+    public String getLatvianGrades(){
+        String grades = "Latvian: ";
+
+        if (Latvian.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Latvian.size(); i < n; i++){
+            grades += Latvian.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all Art grades
+     *
+     * @return a string with all Art grades
+     * or "no grades" if no grades have been added
+     */
+    public String getArtGrades(){
+        String grades = "Art: ";
+
+        if (Art.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Art.size(); i < n; i++){
+            grades += Art.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all Biology grades
+     *
+     * @return a string with all Biology grades
+     * or "no grades" if no grades have been added
+     */
+    public String getBiologyGrades(){
+        String grades = "Biology: ";
+
+        if (Biology.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Biology.size(); i < n; i++){
+            grades += Biology.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all Music grades
+     *
+     * @return a string with all Music grades
+     * or "no grades" if no grades have been added
+     */
+    public String getMusicGrades(){
+        String grades = "Music: ";
+
+        if (Music.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = Music.size(); i < n; i++){
+            grades += Music.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all PhysicalEducation grades
+     *
+     * @return a string with all PhysicalEducation grades
+     * or "no grades" if no grades have been added
+     */
+    public String getPhysicalEducationGrades(){
+        String grades = "Physical Education: ";
+
+        if (PhysicalEducation.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = PhysicalEducation.size(); i < n; i++){
+            grades += PhysicalEducation.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets all English grades
+     *
+     * @return a string with all English grades
+     * or "no grades" if no grades have been added
+     */
+    public String getEnglishGrades(){
+        String grades = "English: ";
+
+        if (English.size() == 0){
+            return grades + "no grades";
+        }
+
+        for (int i = 0, n = English.size(); i < n; i++){
+            grades += English.get(i) + " ";
+        }
+
+        return grades;
+    }
+
+    /**
+     * Gets the final Math grade
+     *
+     * @return a string with the final Math grade
+     * or empty string if no grades have been added
+     */
+    public String getMathFinalGrade(){
+        if (Math.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Math.size();
 
@@ -39,10 +207,20 @@ public class ReportCard {
             finalGrade += Math.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getLiteratureFinalGrade(){
+    /**
+     * Gets the final Literature grade
+     *
+     * @return a string with the final Literature grade
+     * or empty string if no grades have been added
+     */
+    public String getLiteratureFinalGrade(){
+        if (Literature.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Literature.size();
 
@@ -50,10 +228,20 @@ public class ReportCard {
             finalGrade += Literature.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getLatvianFinalGrade(){
+    /**
+     * Gets the final Latvian grade
+     *
+     * @return a string with the final Latvian grade
+     * or empty string if no grades have been added
+     */
+    public String getLatvianFinalGrade(){
+        if (Latvian.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Latvian.size();
 
@@ -61,10 +249,20 @@ public class ReportCard {
             finalGrade += Latvian.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getArtFinalGrade(){
+    /**
+     * Gets the final Art grade
+     *
+     * @return a string with the final Art grade
+     * or empty string if no grades have been added
+     */
+    public String getArtFinalGrade(){
+        if (Art.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Art.size();
 
@@ -72,10 +270,21 @@ public class ReportCard {
             finalGrade += Art.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getBiologyFinalGrade(){
+
+    /**
+     * Gets the final Biology grade
+     *
+     * @return a string with the final Biology grade
+     * or empty string if no grades have been added
+     */
+    public String getBiologyFinalGrade(){
+        if (Biology.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Biology.size();
 
@@ -83,10 +292,20 @@ public class ReportCard {
             finalGrade += Biology.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getMusicFinalGrade(){
+    /**
+     * Gets the final Music grade
+     *
+     * @return a string with the final Music grade
+     * or empty string if no grades have been added
+     */
+    public String getMusicFinalGrade(){
+        if (Music.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = Music.size();
 
@@ -94,10 +313,20 @@ public class ReportCard {
             finalGrade += Music.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getPhysicalEducationFinalGrade(){
+    /**
+     * Gets the final PhysicalEducation grade
+     *
+     * @return a string with the final PhysicalEducation grade
+     * or empty string if no grades have been added
+     */
+    public String getPhysicalEducationFinalGrade(){
+        if (PhysicalEducation.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = PhysicalEducation.size();
 
@@ -105,10 +334,20 @@ public class ReportCard {
             finalGrade += PhysicalEducation.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
-    public int getEnglishFinalGrade(){
+    /**
+     * Gets the final English grade
+     *
+     * @return a string with the final English grade,
+     * or empty string if no grades have been added
+     */
+    public String getEnglishFinalGrade(){
+        if (English.size() == 0){
+            return "";
+        }
+
         int finalGrade = 0;
         int i, n = English.size();
 
@@ -116,41 +355,82 @@ public class ReportCard {
             finalGrade += English.get(i);
         }
 
-        return finalGrade / n;
+        return "Final grade: " + finalGrade / n;
     }
 
     /**
-     * Appends the subject ArrayList with a grade
+     * Appends the Math ArrayList with a grade
      */
     public void setMathGrade(int grade){
         Math.add(grade);
     }
 
+    /**
+     * Appends the Literature ArrayList with a grade
+     */
     public void setLiteratureGrade(int grade){
         Literature.add(grade);
     }
 
+    /**
+     * Appends the Latvian ArrayList with a grade
+     */
     public void setLatvianGrade(int grade){
         Latvian.add(grade);
     }
 
+    /**
+     * Appends the Art ArrayList with a grade
+     */
     public void setArtGrade(int grade){
         Art.add(grade);
     }
 
+    /**
+     * Appends the Biology ArrayList with a grade
+     */
     public void setBiologyGrade(int grade){
         Biology.add(grade);
     }
 
+    /**
+     * Appends the Music ArrayList with a grade
+     */
     public void setMusicGrade(int grade){
         Music.add(grade);
     }
 
+    /**
+     * Appends the PhysicalEducation ArrayList with a grade
+     */
     public void setPhysicalEducationGrade(int grade){
         PhysicalEducation.add(grade);
     }
 
+    /**
+     * Appends the English ArrayList with a grade
+     */
     public void setEnglishGrade(int grade){
         English.add(grade);
+    }
+
+
+    /**
+     * Creates a summary of the report card class.
+     * Each subject on it's own line
+     *
+     * @return report card summary
+     */
+    public  String toString(){
+        String reportCard = getMathGrades() + " " + getMathFinalGrade() + "\n" +
+                getLiteratureGrades() + " " + getLiteratureFinalGrade()+ "\n" +
+                getLatvianGrades() + " " + getLatvianFinalGrade() + "\n" +
+                getArtGrades() + " " + getArtFinalGrade() + "\n" +
+                getBiologyGrades() + " " + getBiologyFinalGrade() + "\n" +
+                getMusicGrades() + " " + getMusicFinalGrade() + "\n" +
+                getPhysicalEducationGrades() + " " + getPhysicalEducationFinalGrade() + "\n" +
+                getEnglishGrades() + " " + getEnglishFinalGrade();
+
+        return reportCard;
     }
 }
